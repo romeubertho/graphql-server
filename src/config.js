38f3b2ec083e3da26b530490f7e3b8dd72bbbd52
root <romeubertho@gmail.com>
2020-03-20 13:10:33 -0300

    change to local mongo

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const databaseConfig = 'mongodb+srv://enfase:enfase&*(@cluster0-etjqi.mongodb.net/quiz';
+// export const databaseConfig = 'mongodb+srv://enfase:enfase&*(@cluster0-etjqi.mongodb.net/quiz';
+export const databaseConfig = 'mongodb://mongo/quiz';
